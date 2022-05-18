61f317252762e2295d6d4143e1b29b8c2d569185
Brent Williams <brent.williams@ddincmail.org>
2022-05-18 16:17:44 -0400

    Removed all SCORM functions into Sco_Interface.js

--- src/ADL_WRAPPER.js
+++ src/ADL_WRAPPER.js
@@ -6,7 +6,8 @@
 * @description Class to specify whether to create a scorm or xAPI  instance for reporting.
 */
 
-//TODO: REFACTOR CLASS CONSTRUCTORS AND INCLUDE SCORM.GET, SCORM.FIND AND ALL OTHER NAME SPACE PROPERTY FUNCTIONS AS CLASS METHODS
+//TODO: Create Seperate Interface Files for SCORM and xAPI. That way this file isn't bloated with multiple API functions
+//NOTE: This CLASS will specificly handle calling the creation/fetch of the API from either Sco_Interface or xAPI_Interface 
 
 // eslint-disable-next-line no-unused-vars
 class ADL_Wrapper
@@ -17,6 +18,7 @@ class ADL_Wrapper
 * @param {String} obj.ADL_TYPE ADL type of reporting. I.E: "SCORM"/"xAPI"
 * @param {Number} obj.ADL_VER Defaults to "2004" ADL version of reporting: ONLY NEEDED FOR SCORM I.E: "2004"/"1.2"
 */
+	//
 	constructor(obj)
 	{
 		this.isSCORM = obj.ADL_TYPE.toUpperCase('SCORM') ? true: false;
@@ -70,125 +72,6 @@ class ADL_Wrapper
 	}
 
 	/**
-	* @description Looks for object named API, starts at current window and searches each parent till found
-	* @author Brent Williams <brent.williams@ddincmail.org> (https://www.github.com/DDincBrent)
-	* @method GetAPI async 
-	* @return {Object} Object is API is found, else returns null
-	*/
-	async GetAPI()
-	{
-		const win = window, 
-			scorm = this.ADL, 
-			find = await this.#FindAPI(), 
-			trace = this.#Trace();
-			
-		let API = null;
-
-		API = find(win);
-
-		if(!API && win.parent && win.parent != win)
-			API = find(win.parent);
-
-		if(!API && win.top && win.top.opener)
-			API = find(win.top.opener);
-
-		if(!API && win.top && win.top.opener && win.top.opener.document)
-			API = find(win.top.opener.document);
-
-		API ? scorm.API.isFound = true : trace(this.msg.failed);
-
-		scorm.API.get = API;
-
-		return API;
-	}
-
-	/**
-	* @description Looks for an object named API
-	* @author Brent Williams <brent.williams@ddincmail.org> (https://www.github.com/DDincBrent).
-	* @method FindAPI
-	* 
-	* @param {Object} window The browser window
-	* @return {Object} Object of API if found, else returns null
-	*/
-	
-	#FindAPI(window)
-	{
-		return new Promise(resolve => 
-		{
-			const scorm = this.ADL;
-			const apiNotFound = (!win.API && !win.API_1484_11), winHasParent = (win.parent), winIsntParent = (win.parent != win), canAttempt = (this.attempts <= this.maxAttempts);
-			
-			let win = window;
-			
-			while(apiNotFound && winHasParent && winIsntParent && canAttempt)
-			{
-				this.attempts++;
-				win = win.parent;
-			}
-
-			resolve(this.#SetScoVersion(scorm.version));
-		});
-	}
-
-	/**
-	* @description Sets the version of scorm set by the user, if no version is specified defaults to 2004
-	* @author Brent Williams <brent.williams@ddincmail.org> (https://www.github.com/DDincBrent).
-	* @method #SetScoVersion
-	* 
-	* @param {String} version A string containing the version number of SCORM desired.
-	* 
-	*/
-	
-	#SetScoVersion(version)
-	{
-		let API = null;
-		const scorm = this.ADL;
-
-		if(version)
-			version == '2004' ? API = window.API_1484_11 : API = window.API;
-
-		if(API)
-		{
-			this.#Trace(`${this.msg.findMsg}: API FOUND: VERSION -- ${scorm.version}`);
-			this.#Trace(`API: ${API}`);
-		}
-		else
-		{
-			this.#Trace(`${this.msg.findMsg}: Error finding API. \nAttempts to find API: ${this.attempts} \nMax Attempts Allowed: ${this.maxAttempts}`);
-		}
-
-		scorm.API.find = API;
-		return API;
-	}
-
-	/**
-	* @description Returns handle for the API
-	* @author Brent Williams <brent.williams@ddincmail.org> (https://www.github.com/DDincBrent).
-	* @method #GetHandle
-	*/
-	
-	#GetHandle()
-	{
-		const API = this.ADL.API;
-
-		if(!API.handle && !API.isFound)  API.handle = this.GetAPI();
-
-		return API.handle;
-	}
-
-	/**
-	* @description Tells LMS to Initialize the connection
-	* @author Brent Williams <brent.williams@ddincmail.org> (https://www.github.com/DDincBrent).
-	* @method Initialize 
-	*/
-	
-	Initialize()
-	{
-		const scorm = this.ADL, debug = scorm.debug;
-		let success = false, completionStatus = scorm.data.completionStatus;
-	}
-
-	/**
 	* @description Sends a message or alert based on debug prefs 
 	* @author Brent Williams <brent.williams@ddincmail.org> (https://www.github.com/DDincBrent)
 	* @method #Trace <PRIVATE>
@@ -205,5 +88,5 @@ class ADL_Wrapper
 	}
 }
 
-const adl_wrapper = new ADL_Wrapper({ ADL_TYPE: 'SCORM', ADL_VER: '2004' });
+//const adl_wrapper = new ADL_Wrapper({ ADL_TYPE: 'SCORM', ADL_VER: '2004' });
 export { ADL_Wrapper };
